d219e6ee5b7390b756c6fa3471ef32a3117aa28d
Atul Dhingra <atul@standard.ai>
2020-10-26 15:06:38 -0700

    create vpc

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("terraform_key.json")
+
+  project = "atuls-playground-228633"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
