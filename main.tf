9b8a08d1c58492bb0c772d4d27b95ea2b26b4a64
Atul Dhingra <atul@standard.ai>
2020-10-27 13:14:56 -0700

    provison infra

--- main.tf
+++ main.tf
@@ -25,6 +25,10 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   tags         = ["web", "dev"]
 
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+
   boot_disk {
     initialize_params {
       image = "cos-cloud/cos-stable"
@@ -42,38 +46,4 @@ resource "google_compute_instance" "vm_instance" {
 
 resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
-}
-
-
-# New resource for the storage bucket our application will use.
-resource "google_storage_bucket" "example_bucket" {
-  name     = "atul-terraform-test"
-  location = "US"
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-# Create a new instance that uses the bucket
-resource "google_compute_instance" "another_instance" {
-  # Tells Terraform that this VM instance must be created only after the
-  # storage bucket has been created.
-  depends_on = [google_storage_bucket.example_bucket]
-
-  name         = "terraform-instance-2"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
 }
